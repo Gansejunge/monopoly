6eadaaf13f3c0a63ad65de1535568929c73bc830
Dennis Kindervater <kindervater@picture-gmbh.de>
2021-02-03 12:38:14 +0100

    feature/dice-rolling - dicerolling

---
+++ src/main/java/dice/Dice.java
@@ -0,0 +1,9 @@
+package dice;
+
+public class Dice {
+    private static RandomNumberGenerator rng = new RandomNumberGenerator(1, 6);
+
+    public static DiceResult roll2Dice(){
+        return new DiceResult(2, rng);
+    }
+}
---
+++ src/main/java/dice/RandomNumberGenerator.java
@@ -0,0 +1,20 @@
+package dice;
+
+import java.security.SecureRandom;
+import java.util.function.IntSupplier;
+
+public class RandomNumberGenerator implements IntSupplier {
+    private SecureRandom random;
+    private int minValue;
+    private int maxValue;
+
+    public RandomNumberGenerator(int minValue, int maxValue){
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+    }
+
+    @Override
+    public int getAsInt() {
+        return minValue + random.nextInt(maxValue);
+    }
+}
