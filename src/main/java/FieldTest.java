7a44e2eb0a28dcbc78e1cb651ee11e8e8df667d0
Dennis Kindervater <kindervater@picture-gmbh.de>
2021-02-10 12:15:32 +0100

    feature/board Einfaches Spielbrett

---
+++ src/main/java/FieldTest.java
@@ -0,0 +1,2 @@
+public class FieldTest { //fixme durch echtes feld ersetzen
+}
---
+++ src/main/java/PlayerTest.java
@@ -0,0 +1,8 @@
+public class PlayerTest { //fixme durch echten Spiel ersetzen
+    public int getPosition(){
+        return 0;
+    }
+
+    public void setPosition(int i){
+    }
+}
