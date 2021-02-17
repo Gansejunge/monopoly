ff31457d3d8adbc65d5ee0876c48175e643ab231
Tobi <tmro@outlook.de>
2021-02-17 12:37:34 +0100

    Erstellung GameController

--- src/main/java/monopoly/GameController.java
+++ src/main/java/monopoly/GameController.java
@@ -1,23 +1,39 @@
 package monopoly;
 
+import java.util.ArrayList;
+
 public class GameController {
-    private Player[] player;
+    private ArrayList<Player> player = new ArrayList<>();
     private int currentPlayer = 0;
 
+    public ArrayList<Player> getPlayer(){
+        return player;
+    }
+
+    public Player getCurrentPlayer(){
+        return player.get(this.currentPlayer);
+    }
+
+    public ArrayList<Player> getOtherPlayer(){
+        ArrayList<Player> otherPlayer = new ArrayList<>(player);
+        otherPlayer.remove(currentPlayer);
+        return otherPlayer;
+    }
+
+    public Player getNextPlayer() {
+        this.currentPlayer++;
+        this.currentPlayer %= this.player.size();
+        return this.player.get(this.currentPlayer);
+    }
+
     public void initPlayer(int amount){
-        this.player = new Player[amount];
         String name = "";
         for(int i = 0; i<amount; i++){
-           this.player[i] = new Player(name);
+           player.add(new Player(name));
         }
     }
 
-    public Player nextPlayer() {
-        this.currentPlayer++;
-        this.currentPlayer %= this.player.length;
-        return this.player[this.currentPlayer];
-    }
-    public void giveMoney(int amountOfMoney){
+    public void transferMoneyToOrFromBank(int amountOfMoney){
     }
 
     public void movePlayer(int amountOfMoves){
@@ -25,4 +41,5 @@ public class GameController {
 
     public void moveToField(int location,boolean getMoneyFromStart){
     }
+
 }
