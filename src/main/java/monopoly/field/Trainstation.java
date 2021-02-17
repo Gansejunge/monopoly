edd363d736af9a26ba7b8efb3d965ea808983cfe
Robin <RLehmkuhl@outlook.de>
2021-02-17 12:50:03 +0100

    Trainstation Rent

--- src/main/java/monopoly/field/Trainstation.java
+++ src/main/java/monopoly/field/Trainstation.java
@@ -1,10 +1,16 @@
 package monopoly.field;
 
+import java.util.ArrayList;
+import math.pow;
+
 public class Trainstation extends Property {
 
     @Override
-    public float getRent() {
-        return 0;
+    public double getRent() {
+        ArrayList<Estate> Estate = owner.getEstate();
+        int count = Estate.size();
+        double price = count > 0 ? Math.pow(25,count-1) : 0;
+        return price;
     }
 
 
