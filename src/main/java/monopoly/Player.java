9d0751e6b60f0677279fdfa226f096bb8a19786f
Florian <florian@example.com>
2021-02-17 12:25:59 +0100

    Expanded Player class

--- src/main/java/monopoly/Player.java
+++ src/main/java/monopoly/Player.java
@@ -4,11 +4,21 @@ import monopoly.deck.Card;
 import monopoly.field.Estate;
 
 public class Player {
-    public String name;
+    private String name;
     private int money = 0;
     private int position = 0;
     private Estate[] ownedEstate;
     private Card[] cards;
+    private int amountOfHouses;
+    private int amountOfHotels;
+
+    public int getAmountOfHouses() {
+        return this.amountOfHouses;
+    }
+
+    public int getAmountOfHotels() {
+        return this.amountOfHotels;
+    }
 
     public Player(String name){
         this.name = name;
@@ -20,5 +30,21 @@ public class Player {
 
     public void setPosition(int position) {
         this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public Estate[] getOwnedEstate() {
+        return ownedEstate;
+    }
+
+    public Card[] getCards() {
+        return cards;
     }
 }
