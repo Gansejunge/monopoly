131e782229144516db8031c4555a5ebe7a3464dd
Dennis Kindervater <kindervater@picture-gmbh.de>
2021-03-03 12:21:55 +0100

    Miete für Bahnhof korrigiert

--- src/main/java/monopoly/field/Trainstation.java
+++ src/main/java/monopoly/field/Trainstation.java
@@ -1,20 +1,13 @@
 package monopoly.field;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-
 public class Trainstation extends Property {
     public Trainstation(String name){
         super(name);
     }
 
     public int getRent() {
-        List<Property> properties = owner.getProperties();
-        properties = properties.stream().filter(p -> p instanceof Trainstation).collect(Collectors.toList());
-        int count = properties.size();
-        double price = count > 0 ? Math.pow(25,count-1) : 0;
-        return (int)price;
+        long count = owner.getProperties().stream().filter(p -> p instanceof Trainstation).count();
+        return (int) (25 * Math.pow(2, count - 1));
     }
 }
 
