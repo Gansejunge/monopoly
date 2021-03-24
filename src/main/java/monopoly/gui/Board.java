6fc39ba707bfc90fc809c2223fd744eb94ece792
Dennis Kindervater <kindervater@picture-gmbh.de>
2021-03-24 21:04:26 +0100

    Felder mit Namen

--- src/main/java/monopoly/gui/Board.java
+++ src/main/java/monopoly/gui/Board.java
@@ -1,18 +1,19 @@
 package monopoly.gui;
 
-import javafx.scene.layout.HBox;
+import javafx.scene.Group;
 
 public class Board {
-    HBox hbox;
+    Group hbox;
 
     public Board(int width, int height){
-        this.hbox = new HBox();
-        this.hbox.setPrefHeight(height);
-        this.hbox.setPrefWidth(width);
-        this.hbox.setStyle("-fx-background-color: #cce3c7");
+//        this.hbox = new HBox();
+//        this.hbox.setPrefHeight(height);
+//        this.hbox.setPrefWidth(width);
+//        this.hbox.setStyle("-fx-background-color: #cce3c7");
+        this.hbox = new Group();
     }
 
-    public HBox getNode(){
+    public Group getNode(){
         return this.hbox;
     }
 }
