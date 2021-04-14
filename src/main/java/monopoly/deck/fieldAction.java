5cf92be3760ab4c7e938f33a4e4f185c796d1e80
Florian Gänsejunge <florian.meergans@livereach.de>
2021-04-14 11:49:43 +0200

    Added empty fieldAction

---
+++ src/main/java/monopoly/deck/fieldAction.java
@@ -0,0 +1,9 @@
+package monopoly.deck;
+
+import monopoly.GameController;
+
+public class fieldAction implements Action{
+    @Override
+    public void perfom(GameController controller) {
+    }
+}
