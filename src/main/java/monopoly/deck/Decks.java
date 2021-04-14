773ed6a4f17887267f8290dcd6b11e924384fba4
Robin <RLehmkuhl@outlook.de>
2021-04-14 12:11:24 +0200

    DeckPrices

--- src/main/java/monopoly/deck/Decks.java
+++ src/main/java/monopoly/deck/Decks.java
@@ -24,23 +24,23 @@ public class Decks {
     public Deck initializeActionCardDeck() {
         Deck actionCards = new Deck();
         actionCards.add(new Card("Rücken Sie vor bis zur Schlossallee.", new MoveToLocation(39, true), CardType.Ereigniskarte));
-        actionCards.add(new Card("Machen Sie einen Ausflug zum Südbahnhof. Wenn Sie über Los kommen, ziehen Sie € 200 ein.", new MoveToLocation(5, true), CardType.Ereigniskarte));
-        actionCards.add(new Card("Ihr Bausparvertrag wird fällig. Sie erhalten € 200.", new GetMoneyAction(200), CardType.Ereigniskarte));
-        actionCards.add(new Card("Rücken Sie vor bis zum Opernplatz. Wenn Sie über Los kommen, ziehen Sie € 200 ein.", new MoveToLocation(24, true), CardType.Ereigniskarte));
+        actionCards.add(new Card("Machen Sie einen Ausflug zum Südbahnhof. Wenn Sie über Los kommen, ziehen Sie € 4000 ein.", new MoveToLocation(5, true), CardType.Ereigniskarte));
+        actionCards.add(new Card("Ihr Bausparvertrag wird fällig. Sie erhalten € 4000.", new GetMoneyAction(4000), CardType.Ereigniskarte));
+        actionCards.add(new Card("Rücken Sie vor bis zum Opernplatz. Wenn Sie über Los kommen, ziehen Sie € 4000 ein.", new MoveToLocation(24, true), CardType.Ereigniskarte));
         actionCards.add(new Card("Rücken Sie vor bis zum nächsten Versorgungswerk. Werfen Sie die Würfel und zahlen dem Eigentümer den zehnfachen\n" +
                 "Betrag Ihres Wurfergebnisses. Wenn das Werk noch niemandem gehört, können Sie es von der Bank kaufen.", new MoveToLocationWhilePossiblyPayingHigherRent(new int[]{12, 28}, 10), CardType.Ereigniskarte));
-        actionCards.add(new Card("Gehen Sie in das Gefängnis. Begeben Sie sich direkt dorthin. Gehen Sie nicht über Los. Ziehen Sie nicht € 200 ein.", new GoToPrison(), CardType.Ereigniskarte));
-        actionCards.add(new Card("Rücken Sie vor bis auf Los. (Ziehe € 200 ein).", new MoveToLocation(0, true), CardType.Ereigniskarte));
-        actionCards.add(new Card("Die Bank zahlt Ihnen eine Dividende von € 50.", new GetMoneyAction(50), CardType.Ereigniskarte));
-        actionCards.add(new Card("Sie lassen Ihre Häuser renovieren. Zahlen Sie: € 25 pro Haus, € 100 pro Hotel.", new MaintainOwnedEstate(25, 100), CardType.Ereigniskarte));
+        actionCards.add(new Card("Gehen Sie in das Gefängnis. Begeben Sie sich direkt dorthin. Gehen Sie nicht über Los. Ziehen Sie nicht € 4000 ein.", new GoToPrison(), CardType.Ereigniskarte));
+        actionCards.add(new Card("Rücken Sie vor bis auf Los. (Ziehe € 4000 ein).", new MoveToLocation(0, true), CardType.Ereigniskarte));
+        actionCards.add(new Card("Die Bank zahlt Ihnen eine Dividende von € 1000.", new GetMoneyAction(1000), CardType.Ereigniskarte));
+        actionCards.add(new Card("Sie lassen Ihre Häuser renovieren. Zahlen Sie: € 500 pro Haus, € 2000 pro Hotel.", new MaintainOwnedEstate(500, 2000), CardType.Ereigniskarte));
         actionCards.add(new Card("Sie kommen aus dem Gefängnis frei! Behalten Sie diese Karte, bis Sie sie benötigen oder verkaufen.", new EscapePrison(), CardType.Ereigniskarte));
-        actionCards.add(new Card("Rücken Sie vor bis zur Seestraße. Wenn Sie über Los kommen, ziehen Sie € 200 ein.", new MoveToLocation(11, true), CardType.Ereigniskarte));
-        actionCards.add(new Card("Sie sind zum Vorstand gewählt worden. Zahlen Sie jedem Spieler € 50.", new RedistributeMoneyToOrFromCurrentPlayer(-50), CardType.Ereigniskarte));
-        actionCards.add(new Card("Ihr Bausparvertrag wird fällig. Sie erhalten € 200.", new GetMoneyAction(200), CardType.Ereigniskarte));
+        actionCards.add(new Card("Rücken Sie vor bis zur Seestraße. Wenn Sie über Los kommen, ziehen Sie € 4000 ein.", new MoveToLocation(11, true), CardType.Ereigniskarte));
+        actionCards.add(new Card("Sie sind zum Vorstand gewählt worden. Zahlen Sie jedem Spieler € 1000.", new RedistributeMoneyToOrFromCurrentPlayer(-1000), CardType.Ereigniskarte));
+        actionCards.add(new Card("Ihr Bausparvertrag wird fällig. Sie erhalten € 4000.", new GetMoneyAction(4000), CardType.Ereigniskarte));
         actionCards.add(new Card("Gehen Sie 3 Felder zurück.", new MoveFields(-3), CardType.Ereigniskarte));
-        actionCards.add(new Card("Strafzettel! Zahlen Sie € 15.", new GetMoneyAction(-15), CardType.Ereigniskarte));
-        actionCards.add(new Card("Rücken Sie vor bis zum nächsten Verkehrsfeld. Der Eigentümer erhält das Doppelte der normalen Miete.\n" +
-                "Wenn das Verkehrsfeld noch niemandem gehört, können Sie es von der Bank kaufen.", new MoveToLocationWhilePossiblyPayingHigherRent(new int[]{5, 15, 25, 35}, 2), CardType.Ereigniskarte));
+        actionCards.add(new Card("Strafzettel! Zahlen Sie € 300.", new GetMoneyAction(-300), CardType.Ereigniskarte));
+        actionCards.add(new Card("Rücken Sie vor bis zum nächsten Bahnhof. Der Eigentümer erhält das Doppelte der normalen Miete.\n" +
+                "Wenn der Bahnhof noch niemandem gehört, können Sie ihn von der Bank kaufen.", new MoveToLocationWhilePossiblyPayingHigherRent(new int[]{5, 15, 25, 35}, 2), CardType.Ereigniskarte));
 
         actionCards.reshuffle();
         return actionCards;
