10be2fa101128fba7094e80ae03f5d89942fb5ca
Marcel L <mluettecke@posteo.net>
2021-04-21 12:18:38 +0200

    Added current player money to card (ugly)

--- src/main/java/monopoly/gui/PlayerSidebar.java
+++ src/main/java/monopoly/gui/PlayerSidebar.java
@@ -7,13 +7,14 @@ import java.util.List;
 
 public class PlayerSidebar {
     VBox vBox;
+
     public PlayerSidebar(int width, int height, List<Player> players){
         this.vBox = new VBox();
         this.vBox.setPrefHeight(height);
         this.vBox.setPrefWidth(width);
 
         for(Player player: players){
-            PlayerCard playerCard = new PlayerCard(height / 4, width, player.getName());
+            PlayerCard playerCard = new PlayerCard(height / 4, width, player.getName(), player.getMoney());
             this.vBox.getChildren().add(playerCard.getNode());
         }
     }
